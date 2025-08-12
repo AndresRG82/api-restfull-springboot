1e675e9612fef7db29d4ebd7b35c8b398385de46
Miguel Ramos <return.infox@gmail.com>
2025-08-11 21:13:19 -0400

    codigo actualizado

---
+++ src/main/java/cl/kibernumacademy/rest/controller/BookController.java
@@ -0,0 +1,46 @@
+package cl.kibernumacademy.rest.controller;
+
+import java.util.List;
+
+import org.springframework.boot.autoconfigure.graphql.GraphQlProperties.Http;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import cl.kibernumacademy.rest.model.Book;
+import cl.kibernumacademy.rest.service.IBookService;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+  
+  private final IBookService bookService;
+
+  public BookController(IBookService bookService) {
+    this.bookService = bookService;
+  }
+
+  @PostMapping
+  public ResponseEntity<Book> createBook(@RequestBody Book book) {
+    Book createdBook = this.bookService.create(book);
+    return new ResponseEntity<>(createdBook, HttpStatus.CREATED);
+  }
+
+  @GetMapping
+  public ResponseEntity<List<Book>> getAllBooks() {
+    return ResponseEntity.ok(this.bookService.findAll());
+  }
+  
+
+
+}
+
+//* POST -> localhost:8080/api/books 
+//* GET -> localhost:8080/api/books
+//* GET -> localhost:8080/api/books/{id}
+//* PUT -> localhost:8080/api/books/{id}
+//* DELETE -> localhost:8080/api/books/{id}
