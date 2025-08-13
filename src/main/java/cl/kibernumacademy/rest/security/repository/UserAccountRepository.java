c7dbf4c46a6ffd13407cb8c39e2382630b2e5e83
Miguel Ramos <return.infox@gmail.com>
2025-08-12 20:32:41 -0400

    codigo de dto

--- src/main/java/cl/kibernumacademy/rest/security/repository/UserAccountRepository.java
+++ src/main/java/cl/kibernumacademy/rest/security/repository/UserAccountRepository.java
@@ -1,5 +1,7 @@
 package cl.kibernumacademy.rest.security.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import cl.kibernumacademy.rest.security.domain.UserAccount;
@@ -7,7 +9,7 @@ import cl.kibernumacademy.rest.security.domain.UserAccount;
 public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
     
     // Método para encontrar un usuario por su nombre de usuario.
-    UserAccount findByUsername(String username);
+    Optional<UserAccount> findByUsername(String username);
     
     // Método para verificar si un usuario existe por su nombre de usuario.
     boolean existsByUsername(String username);
