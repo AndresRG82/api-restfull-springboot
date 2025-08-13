a7ba42ca9ab3e6512da72ad733575dfa116cbbe8
Miguel Ramos <return.infox@gmail.com>
2025-08-12 20:24:14 -0400

    repository

---
+++ src/main/java/cl/kibernumacademy/rest/security/repository/UserAccountRepository.java
@@ -0,0 +1,18 @@
+package cl.kibernumacademy.rest.security.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import cl.kibernumacademy.rest.security.domain.UserAccount;
+
+public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
+    
+    // Método para encontrar un usuario por su nombre de usuario.
+    UserAccount findByUsername(String username);
+    
+    // Método para verificar si un usuario existe por su nombre de usuario.
+    boolean existsByUsername(String username);
+    
+    // Método para eliminar un usuario por su nombre de usuario.
+    void deleteByUsername(String username);
+  
+}
